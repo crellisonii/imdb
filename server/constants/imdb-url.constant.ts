e4d94097c30f74716ccbf6372fe9690b21d4647c
Charles Ellison <crelliso@gmail.com>
2021-11-20 00:26:48 -0600

    add searchNamesurl to constants

--- server/constants/imdb-url.constant.ts
+++ server/constants/imdb-url.constant.ts
@@ -3,3 +3,5 @@ export const baseUrl = "https://imdb-api.com";
 export const searchMovieUrl = "/SearchMovie";
 
 export const searchSeriesUrl = "/SearchSeries";
+
+export const searchNameUrl = "/SearchName";
