b761ee88add48cecff50819338644cad1d7de647
Charles Ellison <crelliso@gmail.com>
2021-11-20 00:55:32 -0600

    add getCompany query to resolver

--- server/modules/search/resolver.ts
+++ server/modules/search/resolver.ts
@@ -2,6 +2,7 @@ import { Arg, Query, Resolver } from "type-graphql";
 import { SearchInput } from ".";
 import { green, log, magenta, red, yellow } from "../../helpers";
 import {
+  getSearchCompanyUrl,
   getSearchEpisodesUrl,
   getSearchMoviesUrl,
   getSearchNamesUrl,
@@ -12,14 +13,14 @@ import { SearchData } from "./types";
 
 @Resolver()
 export class SearchResolver {
-  @Query(returns => SearchData, { description: "Search movies by titles" })
-  async searchMovies(
+  @Query(returns => SearchData, { description: "Search companies by name" })
+  async searchCompany(
     @Arg("searchInput") input: SearchInput
   ): Promise<SearchData | string> {
     try {
       log(magenta("Search input: "), magenta(JSON.stringify(input)));
       const { expression, language } = input;
-      const url = getSearchMoviesUrl(expression, language);
+      const url = getSearchCompanyUrl(expression, language);
       const options: AxiosRequestConfig = {
         url,
         method: "GET",
@@ -33,14 +34,35 @@ export class SearchResolver {
     }
   }
 
-  @Query(returns => SearchData, { description: "Search tv shows by title" })
-  async searchSeries(
+  @Query(returns => SearchData, { description: "Search tv episodes by title" })
+  async searchEpisodes(
     @Arg("searchInput") input: SearchInput
   ): Promise<SearchData | string> {
     try {
       log(magenta("Search input: "), magenta(JSON.stringify(input)));
       const { expression, language } = input;
-      const url = getSearchSeriesUrl(expression, language);
+      const url = getSearchEpisodesUrl(expression, language);
+      const options: AxiosRequestConfig = {
+        url,
+        method: "GET",
+      };
+      log(green("search options: "), green(JSON.stringify(options)));
+      const resp = await axios(options);
+      return resp.data;
+    } catch (e) {
+      log(red("Search Error: "), red(JSON.stringify(e)));
+      throw new Error("Error");
+    }
+  }
+
+  @Query(returns => SearchData, { description: "Search movies by titles" })
+  async searchMovies(
+    @Arg("searchInput") input: SearchInput
+  ): Promise<SearchData | string> {
+    try {
+      log(magenta("Search input: "), magenta(JSON.stringify(input)));
+      const { expression, language } = input;
+      const url = getSearchMoviesUrl(expression, language);
       const options: AxiosRequestConfig = {
         url,
         method: "GET",
@@ -75,14 +97,14 @@ export class SearchResolver {
     }
   }
 
-  @Query(returns => SearchData, { description: "Search tv episodes by title" })
-  async searchEpisodes(
+  @Query(returns => SearchData, { description: "Search tv shows by title" })
+  async searchSeries(
     @Arg("searchInput") input: SearchInput
   ): Promise<SearchData | string> {
     try {
       log(magenta("Search input: "), magenta(JSON.stringify(input)));
       const { expression, language } = input;
-      const url = getSearchEpisodesUrl(expression, language);
+      const url = getSearchSeriesUrl(expression, language);
       const options: AxiosRequestConfig = {
         url,
         method: "GET",
