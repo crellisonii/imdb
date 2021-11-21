848f0e04cc83255d29007b6a4f10f9986eb76fe7
Charles Ellison <crelliso@gmail.com>
2021-11-21 01:41:28 -0600

    update comments

--- server/modules/image/resolver.ts
+++ server/modules/image/resolver.ts
@@ -12,18 +12,18 @@ export class ImageResolver {
     @Arg("imageInput") input: ImageInput
   ): Promise<ImageData | string> {
     try {
-      log(magenta("FullCast input: "), magenta(JSON.stringify(input)));
+      log(magenta("Image input: "), magenta(JSON.stringify(input)));
       const { id, language, options } = input;
       const url = getImageUrl(id, language, options);
       const config: AxiosRequestConfig = {
         url,
         method: "GET",
       };
-      log(green("FullCast options: "), green(JSON.stringify(config)));
+      log(green("Image options: "), green(JSON.stringify(config)));
       const resp = await axios(config);
       return resp.data;
     } catch (e) {
-      log(red("FullCast Error: "), red(JSON.stringify(e)));
+      log(red("Image Error: "), red(JSON.stringify(e)));
       throw new Error("Error");
     }
   }
--- server/modules/trailer/resolver.ts
+++ server/modules/trailer/resolver.ts
@@ -12,18 +12,18 @@ export class TrailerResolver {
     @Arg("trailerInput") input: TrailerInput
   ): Promise<TrailerData | string> {
     try {
-      log(magenta("FullCast input: "), magenta(JSON.stringify(input)));
+      log(magenta("Trailer input: "), magenta(JSON.stringify(input)));
       const { id, language } = input;
       const url = getTrailerUrl(id, language);
       const config: AxiosRequestConfig = {
         url,
         method: "GET",
       };
-      log(green("FullCast options: "), green(JSON.stringify(config)));
+      log(green("Trailer options: "), green(JSON.stringify(config)));
       const resp = await axios(config);
       return resp.data;
     } catch (e) {
-      log(red("FullCast Error: "), red(JSON.stringify(e)));
+      log(red("Trailer Error: "), red(JSON.stringify(e)));
       throw new Error("Error");
     }
   }
