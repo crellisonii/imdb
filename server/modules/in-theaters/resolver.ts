2a568692ab98d50978afd840abfeb9cc7c90df3f
Charles Ellison <crelliso@gmail.com>
2021-12-06 00:35:37 -0600

    create in theater module

---
+++ server/modules/in-theaters/resolver.ts
@@ -0,0 +1,27 @@
+import { Arg, Query, Resolver } from "type-graphql";
+import { NewMovieData } from ".";
+import { buildUrl } from "../../helpers";
+import { cyan, green, log, magenta, red } from "../../utils";
+import { AxiosRequestConfig } from "axios";
+import { getImdbService } from "../../services";
+
+@Resolver()
+export class InTheaterResolver {
+  @Query(returns => NewMovieData)
+  async getNewMovies(
+    @Arg("newMovieInput", { nullable: true }) input: string = "en"
+  ): Promise<NewMovieData | string> {
+    try {
+      log(magenta("In Theater input"), input);
+      const url = buildUrl(input, "/InTheaters");
+      log(cyan("In Theater url: "), url);
+      const config: AxiosRequestConfig = { url };
+      log(green("In Theater config: "), config);
+      const resp = await getImdbService<NewMovieData>(config);
+      return resp.data;
+    } catch (e) {
+      log(red("In Theater error:"), e);
+      throw new Error("error");
+    }
+  }
+}
